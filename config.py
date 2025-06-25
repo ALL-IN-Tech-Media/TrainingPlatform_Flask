f3bea72d3e4f764bb35ce2c9c7467ef6815861a4
Zixiao Wang <sw319346@gmail.com>
2025-06-25 14:47:26 +0800

    修改数据集和模型存储的位置

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 AIDJANGO_URL = "http://81.69.223.133:58097" # Django后端地址
-LOCAL_DATASET_DIR = '/tmp/AIData/Datasets' # 本地数据集目录
-LOCAL_PRE_MODEL_DIR = '/tmp/AIData/model_factory' # 本地预训练模型目录
+LOCAL_DATASET_DIR = '/home/ooin/ooin_training/NLPData' # 本地数据集目录
+LOCAL_PRE_MODEL_DIR = '/home/ooin/ooin_training/model_factory' # 本地预训练模型目录
 MINIO_SERVER_PORT = "192.168.31.138:9000" # MinIO服务器地址
 
 MINIO_ACCESS_KEY = "admin"
